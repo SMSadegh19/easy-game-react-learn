62b80b8f61086f566207575a5cd47271e092ccd2
Sajad F. Maghrebi <sajad.faghfoor@gmai.com>
2022-01-15 17:46:35 +0330

    game without time travel works

--- src/index.js
+++ src/index.js
@@ -1,17 +1,111 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function calculateWinner(sq, upperState) {
+  console.log("calculateWinner", upperState);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  // console.log(sq);
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (sq[a] && sq[a] === sq[b] && sq[a] === sq[c]) {
+      upperState.end = true;
+      return sq[a];
+    }
+  }
+  return null;
+}
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sq: Array(9).fill(null),
+      xIsNext: true,
+      end: false,
+    };
+  }
+
+  handleClick(i) {
+    // console.log("in handle click", i);
+    const sq = this.state.sq.slice();
+    if (this.state.end || sq[i]) {
+      return;
+    }
+    sq[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({ sq: sq, xIsNext: !this.state.xIsNext });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square value={this.state.sq[i]} onClick={() => this.handleClick(i)} />
+    );
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.sq, this.state);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+ReactDOM.render(<Game />, document.getElementById("root"));
